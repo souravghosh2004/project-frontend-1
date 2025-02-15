ea389c4db25e22c5ec84f9fad44ceeebfebb5f0c
souravghosh2004 <souravghoshsonakuri@gmail.com>
2025-02-15 14:58:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  
-  server : {
-    proxy : {
-      '/api' : 'https://project-backend-orpin.vercel.app/'
-    }
-  },
+
 
   plugins: [react()],
 })
