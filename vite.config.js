03826034aadbaa336821942a767dbbae4133c267
Suvajit Ghosh <suvajitghosh344@gmail.com>
2025-02-15 23:27:50 +0530

    change url

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  server : {
-    proxy : {
-      '/api' : 'https://project-backend-orpin.vercel.app/'
-    }
-  },
-
-  plugins: [react()],
-})
-
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 
 // // https://vite.dev/config/
 // export default defineConfig({
-
-
+//   server : {
+//     proxy : {
+//       '/api' : 'https://project-backend-orpin.vercel.app/'
+//     }
+//   },
 
 //   plugins: [react()],
 // })
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+
+
+
+  plugins: [react()],
+})
 
