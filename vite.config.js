6e54bd89a424e9c7783b624b5baff9e25f8939ea
Suvajit Ghosh <suvajitghosh344@gmail.com>
2025-02-15 14:30:06 +0530

    vite config change

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,23 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+  
+//   server : {
+//     proxy : {
+//       '/api' : 'https://project-backend-orpin.vercel.app/'
+//     }
+//   },
+
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  
-  server : {
-    proxy : {
-      '/api' : 'https://project-backend-orpin.vercel.app/'
-    }
-  },
-
   plugins: [react()],
 })
+
